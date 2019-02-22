7b839344d0321d29d60e983de03a62fa9226df37
ImeldaZahwaAracella27rpl <46397825+ImeldaZahwaAracella27rpl@users.noreply.github.com>
2019-02-22 11:42:03 +0700

    Tugas Modul 3

---
+++ login.php
@@ -0,0 +1,9 @@
+<?php
+ $user = $_POST['na']; 
+ $pass = $_POST['psw']; 
+    if ($user == "Imelda" && $pass == "200802") {     
+            echo "Login Berhasil"; 
+        } else {     
+            echo "Login Gagal";
+        } 
+?> 
